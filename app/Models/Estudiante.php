5e447da18da9b8d33bbaefedcc0873e9a510db7c
niicodeer <nicoradin.job@gmail.com>
2023-12-05 17:25:52 -0300

    #3 migracion, modelo, facorty, seeder de estudiante

---
+++ app/Models/Estudiante.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estudiante extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'nombre',
+        'apellido',
+        'email',
+        'cuil',
+        'fecha_nac',
+        'esAlumno',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory()->create();
+        $this->call([
+            EstudianteSeeder::class,
+        ]);
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
