835d21122dd3852c275447cc01f68b9c07517cab
LucasCis92 <lucascis1992@gmail.com>
2023-12-06 10:39:36 -0300

    #5 - Creación del modelo y migración de Tutor.

---
+++ app/Models/Tutor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tutor extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'cuit',
+        'telefono',
+        'nombre',
+        'apellido',
+        'ocupacion',
+    ];
+}
