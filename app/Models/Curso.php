7036dcdc1f862d1deb350dffd45696d0010af0a6
LucasCis92 <lucascis1992@gmail.com>
2023-12-06 17:06:00 -0300

    #8, creada migración y seeders para Curso

---
+++ app/Models/Curso.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'turno',
+        'añoCurso',
+        'division',
+        'cantidadAlumnos',
+        'cantidadMaxima',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create();
         $this->call([
             EstudianteSeeder::class,
+            CursoSeeder::class,
         ]);
 
 
