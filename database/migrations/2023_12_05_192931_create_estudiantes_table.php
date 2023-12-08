eb9800f4f4c89b09cd2e903fa2d5b8d9b8be638c
Nico Radin <nicoradin.job@gmail.com>
2023-12-08 18:10:34 -0300

    deleted foreign on estudiantes migration

--- database/migrations/2023_12_05_192931_create_estudiantes_table.php
+++ database/migrations/2023_12_05_192931_create_estudiantes_table.php
@@ -19,8 +19,6 @@ return new class extends Migration
             $table->string('email', 100);
             $table->date('fecha_nac')->nullable();
             $table->boolean('esAlumno')->default(false);
-            $table->unsignedBigInteger('dato_id');
-            $table->foreignId('user_id')->references('id')->on('dato_estudiantes');
             $table->timestamps();
         });
     }
