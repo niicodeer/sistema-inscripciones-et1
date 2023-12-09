9756006911c46462c369d816f31b652ec3efb4d7
LucasCis92 <lucascis1992@gmail.com>
2023-12-09 18:02:19 -0300

    #16 - relacionado tutor con estudiante

--- database/migrations/2023_12_08_203509_add_foreign_key_estudiante.php
+++ database/migrations/2023_12_08_203509_add_foreign_key_estudiante.php
@@ -14,6 +14,8 @@ return new class extends Migration
     Schema::table('estudiantes', function (Blueprint $table) {
         $table->unsignedBigInteger('dato_id')->nullable();
         $table->foreign('dato_id')->references('id')->on('dato_estudiantes')->onUpdate('set null')->onDelete('set null');
+        $table->unsignedBigInteger('tutor_id')->nullable();
+        $table->foreign('tutor_id')->references('id')->on('tutores')->onUpdate('set null')->onDelete('set null');
     });
 }
 
@@ -25,6 +27,8 @@ return new class extends Migration
     Schema::table('estudiantes', function (Blueprint $table) {
         $table->dropForeign(['dato_id']);
         $table->dropColumn('dato_id');
+        $table->dropForeign(['tutor_id']);
+        $table->dropColumn('tutor_id');
     });
 }
 };
