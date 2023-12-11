10b5bf79be4759348d0ede444882475c140b2e82
JuanAlvarez92 <ajjuaan@gmail.com>
2023-12-11 18:20:52 -0300

    #23-curso resource

---
+++ app/Filament/Resources/CursoResource/Pages/EditCurso.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CursoResource\Pages;
+
+use App\Filament\Resources\CursoResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCurso extends EditRecord
+{
+    protected static string $resource = CursoResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
