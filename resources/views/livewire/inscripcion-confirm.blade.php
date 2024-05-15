83dd576ce263b1631b08618304e73d54ef555502
niicodeer <nicoradin.job@gmail.com>
2024-05-15 01:47:30 -0300

    #65 - confirmacion isncripcion y modal (WIP)

---
+++ resources/views/livewire/inscripcion-confirm.blade.php
@@ -0,0 +1,22 @@
+@section('title', 'Inscripción correcta')
+<div class="flex flex-col gap-8 justify-center items-center w-full max-w-96 text-center">
+
+    <h1 class="text-2xl md:text-3xl font-bold">Tu inscripción ha sido registrada</h1>
+    <p class="">Gracias por inscribirte en nuestra institución! Ya recibimos tu solicitud y está en proceso de
+        revisión.
+        <br />
+        <br />
+        En las próximas horas recibirás un mail al correo que proporcionaste con el estado de tu solicitud y con la
+        fecha y hora en la que debes presentarte con la documentación requerida. ¡Recordá comprobar tu bandeja de
+        entrada como la bandeja de Spam!
+        <br />
+        <br />
+        Puedes cerrar la ventana o inscribir a otra persona en caso que lo necesites
+    </p>
+    <div class="flex gap-2 flex-col items-center">
+        <a class="p-4 text-base font-bold text-[#202020] bg-[#EA9010] max-w-80 w-full rounded-md hover:bg-opacity-80 shadow-md "
+            href="{{ route('inicio') }}">
+            Volver al inicio
+        </a>
+    </div>
+</div>
