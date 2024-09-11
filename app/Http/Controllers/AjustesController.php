23fb5ae949cbdd9c7b5509974c941ebc39640d22
niicodeer <nicoradin.job@gmail.com>
2024-09-11 18:42:06 -0300

    fix $diferenciaDiasInscripcion

--- app/Http/Controllers/AjustesController.php
+++ app/Http/Controllers/AjustesController.php
@@ -29,23 +29,13 @@ class AjustesController extends Controller
             $finInscripcion = $ajustes->fin_fecha_inscripcion . ' ' . $ajustes->fin_hora_inscripcion;
             $now = Carbon::now();
 
-            /**
-             **Casos:
-             **inscripciones habilitadas pero falta para la fecha y horario -> 'faltan tantos dias y horas para ...'
-             **inscripciones habilitadas dentro de la fecha y horario -> permite realizar el proceso
-             **inscripciones habilitadas pasadas de la fecha y horario -> 'el proceso de... ha finalizado. nos vemos en la prox. convocatoria
-             **inscripciones deshabilitadas -> 'No se encuentra habilitada la convocatoria...'
-
-             ** Muestro o no los botones einfo en la pantalla principal,
-             ** o muestro los botones y dentro de cada página me dice si estan o no habilitadas las inscripciones y pre..
-             **/
             if ($ajustes->habilitar_preinscripcion) {
                 if ($now->between($inicioPreinscripcion, $finPreinscripcion)) {
                     $preinscripcionHabilitada = true;
                 } elseif ($now < $inicioPreinscripcion) {
                     $diferenciaDiasPreinscripcion = $now->locale('es')->diffForHumans($inicioPreinscripcion, CarbonInterface::DIFF_ABSOLUTE, false);
                 } else {
-                    $diferenciaDiasPreinscripcion = -$now->locale('es')->diffForHumans($finPreinscripcion, CarbonInterface::DIFF_ABSOLUTE, false);
+                    $diferenciaDiasPreinscripcion = -1;
                 }
                 $preinscripcionHabilitada = true;
             }
@@ -56,7 +46,7 @@ class AjustesController extends Controller
                 } elseif ($now < $inicioInscripcion) {
                     $diferenciaDiasInscripcion = $now->locale('es')->diffForHumans($inicioInscripcion, CarbonInterface::DIFF_ABSOLUTE, false);
                 } else {
-                    $diferenciaDiasInscripcion = -$now->locale('es')->diffForHumans($finInscripcion, CarbonInterface::DIFF_ABSOLUTE, false);
+                    $diferenciaDiasInscripcion = -1;
                 }
                 $inscripcionHabilitada = true;
             }
